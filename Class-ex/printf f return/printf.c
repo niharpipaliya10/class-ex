696a251c542b28d9480d49b762dc0833e90593db
Nihar Pipaliya <114608191+niharpipaliya10@users.noreply.github.com>
2025-04-17 07:52:06 +0530

    Add files via upload

---
+++ Class-ex/printf f return/printf.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+int i=10;
+printf("i");
+//i=printf("hello\n");
+//printf(printf("%d\n",printf("nihar\n")));
+
+
+}
---
+++ Class-ex/printf f return/scanf.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+
+long int ch;
+ ch = scanf("%ld",&ch);
+printf("ch=%ld\n",ch);
+
+}
